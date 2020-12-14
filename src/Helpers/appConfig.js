23beb246869c2033bd3179bcebd5aa99c9ad111f
khu23 <you@example.com>
2020-12-14 09:32:44 +0530

    update

---
+++ src/Helpers/appConfig.js
@@ -0,0 +1,10 @@
+const appConfig = {
+    baseUrl: "http://app.membroz.com/api/",
+    headers: {
+        'Content-Type': 'application/json',
+        'authkey': "5fd3058ac74c3d5ba270f1f6"
+    }
+}
+
+
+export default appConfig;
