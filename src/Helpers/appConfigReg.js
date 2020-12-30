f45ed9575cd9fd68650923ee724a8f22fbd7d7bc
HarshadJariwala <harshadjariwala1998@gmail.com>
2020-12-30 16:44:34 +0530

    update

---
+++ src/Helpers/appConfigReg.js
@@ -0,0 +1,9 @@
+const appConfigReg = {
+    baseUrl: "https://live.membroz.com/api/",
+    headers: {
+        'Content-Type': 'application/json',
+        'authkey': "5fd3058ac74c3d5ba270f1f6"
+    }
+}
+
+export default appConfigReg;
